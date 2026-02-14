9d134b2bc62d3c8f041be9e3b541842bbc64b9c7
Aiperi Zhenishova <zhenishovaaiperi@gmail.com>
2026-02-14 12:30:27 +0600

    update

--- script.js
+++ script.js
@@ -75,14 +75,20 @@ document.addEventListener('DOMContentLoaded', () =>{
         undraw()
         currentPosition += width
         draw()
+        freeze()
     }
 
 
 
     // freeze function 
     function freeze(){
-        if(current.some(index => squares[currentPosition + index + width].classList.contains('taken'))) {
+        if(current.some(index => squares[currentPosition + index + width].classList.contains('taken'))){
             current.forEach(index => squares[currentPosition + index].classList.add('taken'))
+            // start a new tetromino falling
+            random = Math.floor(Math.random() * theTetronomies.length)
+            current = theTetronomies[random][currentRotaion]
+            currentPosition = 4
+            draw()
         }
     }
 })
